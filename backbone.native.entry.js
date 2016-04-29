dd1c1085e120e3d09a06163799c8b91115984eed
juanca <juanca.med@gmail.com>
2016-04-29 01:11:19 -0700

    Extract off into own helper file

--- backbone.native.entry.js
+++ backbone.native.entry.js
@@ -127,48 +127,7 @@
         });
     }
 
-    /**
-     * Remove an event handler from an element.
-     *
-     * @param {Element} parentElement The element to unbind event handlers from.
-     * @param {string} eventName (Optional) The event to unbind, e.g. 'click'.
-     * @param {string} selector (Optional) The selector to unbind.
-     * @param {function(Event, Element)} callback (Optional) The function to unbind.
-     */
-    function off(parentElement, eventName, selector, callback){
-        if (typeof selector === 'function'){
-            callback = selector;
-            selector = null;
-        }
-
-        var parts = namespaceRE.exec(eventName || '');
-        eventName = parts[1];
-        var namespace = parts[2];
-        var handlers = handlersFor(parentElement) || [];
-
-        if (!eventName && !namespace && !selector && !callback){
-            // Fastpath to remove all handlers.
-            handlers.forEach(function(item){
-                parentElement.removeEventListener(item.eventName, item.handler, false);
-            });
-            clearHandlers(parentElement);
-        } else {
-            var matchedHandlers = handlers.filter(function(item){
-                return ((!namespace || item.namespace === namespace) &&
-                    (!eventName || item.eventName === eventName) &&
-                    (!callback || item.callback === callback) &&
-                    (!selector || item.selector === selector));
-            });
-
-            matchedHandlers.forEach(function(item){
-                parentElement.removeEventListener(item.eventName, item.handler, false);
-
-                handlers.splice(handlers.indexOf(item), 1);
-            });
-
-            if (handlers.length === 0) clearHandlers(parentElement);
-        }
-    }
+    var off = require('./lib/jquery-interface/helpers/off.js')(cacheKeyProp, id, handlers, unusedKeys);
 
     /**
      * Construct a new jQuery-style element representation.
