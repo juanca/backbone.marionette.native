be6360496f247b65e968b90281e1dfd735925c9b
juanca <juanca.med@gmail.com>
2016-04-29 01:21:16 -0700

    Extract on into own helper file

--- backbone.native.entry.js
+++ backbone.native.entry.js
@@ -45,14 +45,6 @@
 
     var namespaceRE = require('./lib/jquery-interface/helpers/namespace-regular-expression.js');
 
-    var matchesSelector = Element.prototype.matchesSelector || null;
-    if (!matchesSelector){
-        ['webkit', 'moz', 'o', 'ms'].forEach(function(prefix){
-            var func = Element.prototype[prefix + 'MatchesSelector'];
-            if (func) matchesSelector = func;
-        });
-    }
-
     // The element property to save the cache key on.
     var cacheKeyProp = 'backboneNativeKey' + Math.random();
     var id = 1;
@@ -63,69 +55,7 @@
 
     var clearHandlers = require('./lib/jquery-interface/helpers/clear-handlers.js')(cacheKeyProp, id, handlers, unusedKeys);
 
-    /**
-     * Add event handlers to an element.
-     *
-     * @param {Element} parentElement The element to bind event handlers to.
-     * @param {string} eventName The event to bind, e.g. 'click'.
-     * @param {string} selector (Optional) The selector to match when an event propagates up.
-     * @param {function(Event, Element)} callback The function to call when the event is fired.
-     */
-    function on(parentElement, eventName, selector, callback){
-        // Adjust arguments if selector was not provided.
-        if (typeof selector === 'function'){
-            callback = selector;
-            selector = null;
-        }
-
-        var parts = namespaceRE.exec(eventName);
-        eventName = parts[1] || null;
-        var namespace = parts[2] || null;
-
-        if (!eventName) return;
-
-        var handler = callback;
-        var originalCallback = callback;
-        if (selector){
-            // Event delegation handler to match a selector for child element events.
-            handler = function(event){
-                for (var el = event.target; el && el !== parentElement; el = el.parentElement){
-                    if (matchesSelector.call(el, selector)){
-                        // jQuery does not include the second argument, but we have included it
-                        // for simplicity because 'this' will likely be bound to the view inside
-                        // the callback, and as noted above, we cannot override 'currentTarget'.
-                        var result = originalCallback.call(el, event, el);
-                        if (result === false){
-                            event.stopPropagation();
-                            event.preventDefault();
-                        }
-                        return result;
-                    }
-                }
-            };
-        } else {
-            // Standard event handler bound directly to the element.
-            handler = function(event){
-                var result = originalCallback.call(parentElement, event, parentElement);
-                if (result === false){
-                    event.stopPropagation();
-                    event.preventDefault();
-                }
-                return result;
-            };
-        }
-
-        parentElement.addEventListener(eventName, handler, false);
-
-        // Save event handler metadata so that the handler can be unbound later.
-        handlersFor(parentElement).push({
-            eventName: eventName,
-            callback: callback,
-            handler: handler,
-            namespace: namespace,
-            selector: selector
-        });
-    }
+    var on = require('./lib/jquery-interface/helpers/on.js')(cacheKeyProp, id, handlers, unusedKeys);
 
     var off = require('./lib/jquery-interface/helpers/off.js')(cacheKeyProp, id, handlers, unusedKeys);
 
