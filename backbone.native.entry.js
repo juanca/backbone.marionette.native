cf46096859ce35e192b4ecc6638ad2d7abdece3e
juanca <juanca.med@gmail.com>
2016-04-29 01:09:40 -0700

    Extract handlerFor into own helper file

--- backbone.native.entry.js
+++ backbone.native.entry.js
@@ -59,25 +59,7 @@
     var handlers = {};
     var unusedKeys = [];
 
-    /**
-     * Get the event handlers for a given element, creating an empty set if one doesn't exist.
-     *
-     * To avoid constantly filling the handlers object with null values, we reuse old IDs that
-     * have been created and then cleared.
-     *
-     * @param {Element} el The element to get handlers for.
-     *
-     * @return {Array} An array of handlers.
-     */
-    function handlersFor(el){
-        if (!el[cacheKeyProp]){
-            // Pick a new key, from the unused pool, or make a new one.
-            el[cacheKeyProp] = unusedKeys.length === 0 ? ++id : unusedKeys.pop();
-        }
-
-        var cacheKey = el[cacheKeyProp];
-        return handlers[cacheKey] || (handlers[cacheKey] = []);
-    }
+    var handlersFor = require('./lib/jquery-interface/helpers/handlers-for.js')(cacheKeyProp, id, handlers, unusedKeys);
 
     /**
      * Clear the event handlers for a given element.
