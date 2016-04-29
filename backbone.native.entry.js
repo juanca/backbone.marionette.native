2bbfc5b429eaea1a243f620e670532709f16e0a5
juanca <juanca.med@gmail.com>
2016-04-29 01:12:12 -0700

    Extract $.prototype.remove into own file

--- backbone.native.entry.js
+++ backbone.native.entry.js
@@ -198,32 +198,7 @@
 
         attr: require('./lib/jquery-interface/attr.js'),
         html: require('./lib/jquery-interface/html.js'),
-
-        /**
-         * Remove an element from the DOM and remove all event handlers bound to it and
-         * its child elements.
-         *
-         * Used in Backbone.View.prototype.remove.
-         *
-         * @return {$} This instance.
-         */
-        remove: function(){
-            var el = this[0];
-            if (el.parentElement) el.parentElement.removeChild(el);
-
-            // Unbind all event handlers on the element and children.
-            (function removeChildEvents(element){
-                off(element);
-
-                for (var i = 0, len = element.childNodes.length; i < len; i++){
-                    if (element.childNodes[i].nodeType !== Node.TEXT_NODE){
-                        removeChildEvents(element.childNodes[i]);
-                    }
-                }
-            })(el);
-
-            return this;
-        },
+        remove: require('./lib/jquery-interface/remove.js')(cacheKeyProp, id, handlers, unusedKeys),
 
         /**
          * Bind an event handler to this element.
