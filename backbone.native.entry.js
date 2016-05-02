e7e2d7a79d51bf79725887cf0e9dc4e184b1ee39
juanca <juanca.med@gmail.com>
2016-05-01 23:20:12 -0700

    Remove setting Backbone.Native on root
    For now... Exporting or setting on root might be brought back with spec coverage at a later time.

--- backbone.native.entry.js
+++ backbone.native.entry.js
@@ -5,27 +5,5 @@
 
   $.ajax = require('./lib/ajax.js');
 
-  if (typeof exports !== 'undefined') {
-    module.exports = $;
-  } else {
-    var root = this;
-    var originalBackboneNative = root.Backbone ? root.Backbone.Native : null;
-    var original$ = root.$;
-    if (root.Backbone) root.Backbone.Native = $;
-    root.$ = $;
-
-    $.noConflict = function(deep){
-      root.$ = original$;
-      if (deep) root.Backbone.Native = originalBackboneNative;
-      return $;
-    };
-
-    if (root.Backbone){
-      if (root.Backbone.setDomLibrary){ // v0.9.2
-        root.Backbone.setDomLibrary($);
-      } else { // v1.0.0
-        root.Backbone.$ = $;
-      }
-    }
-  }
+  module.exports = $;
 }).call(this);
